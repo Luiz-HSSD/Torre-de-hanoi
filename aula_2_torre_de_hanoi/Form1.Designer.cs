51239497fbc01bf0b49bcf4af46eeab413722539
Luiz Henrique Santos Sousa Diniz <lhenrique_diniz@hotmail.com>
2017-03-29 19:00:34 -0300

    implementação 1.0

---
+++ aula_2_torre_de_hanoi/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace aula_2_torre_de_hanoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "resultado ótimo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(62, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 64);
+            this.label1.TabIndex = 1;
+            this.label1.UseCompatibleTextRendering = true;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(102, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 57);
+            this.label2.TabIndex = 2;
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label2_MouseDown);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
+            this.label2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label2_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(112, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 47);
+            this.label3.TabIndex = 3;
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label3_MouseDown);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label3_MouseMove);
+            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label3_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 464);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Refazer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(165, 337);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(124, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Passos:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(965, 529);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Torre de hanoi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ aula_2_torre_de_hanoi/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace aula_2_torre_de_hanoi
+{
+    public partial class Form1 : Form
+    {
+        private Point FirstPoint1 = new Point(62,206);
+        private Point FirstPoint2 = new Point(102, 149);
+        private Point FirstPoint3 = new Point(112, 102);
+        private Point MyFuckingPoint = new Point();
+        private Point[] BackupPoint = new Point[] 
+        {
+            new Point(62, 206),
+            new Point(102,149),
+            new Point(112,102)
+        };
+        private int passos=new int();
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = passos.ToString();
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (label1.Location.X + 40 != label2.Location.X  && label1.Location.X + 50 != label3.Location.X )
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    FirstPoint1 = e.Location;
+                }
+            }
+        }
+
+        private void label2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left && label3.Location.X != label2.Location.X + 10)
+            {
+                FirstPoint2 = e.Location;
+            }
+
+        }
+
+        private void label3_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                FirstPoint3 = e.Location;
+            }
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left && label1.Location.X + 40 != label2.Location.X && label1.Location.X + 50 != label3.Location.X)
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    label1.Left = e.X + label1.Left - FirstPoint1.X;
+                    label1.Top = e.Y + label1.Top - FirstPoint1.Y;
+                }
+            }
+        }
+
+        private void label2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left && label3.Location.X != label2.Location.X + 10)
+            {
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    label2.Left = e.X + label2.Left - FirstPoint2.X;
+                    label2.Top = e.Y + label2.Top - FirstPoint2.Y;
+                }
+            }
+
+        }
+
+        private void label3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                label3.Left = e.X + label3.Left - FirstPoint3.X;
+                label3.Top = e.Y + label3.Top - FirstPoint3.Y;
+            }
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            passos = 0;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            reiniciar();
+            Thread.Sleep(2000);
+
+            //levar final o menor
+            MyFuckingPoint.X = 795;
+            MyFuckingPoint.Y = 234;
+
+            label3.Location = MyFuckingPoint;
+            passos++;
+            Thread.Sleep(3000);
+            //levar  meio médio
+            MyFuckingPoint.X = 421;
+            MyFuckingPoint.Y = 213;
+            label2.Location = MyFuckingPoint;
+            
+
+            Thread.Sleep(3000);
+            //
+            MyFuckingPoint.X = 445;
+            MyFuckingPoint.Y = 166;
+            label3.Location = MyFuckingPoint;
+            
+
+            Thread.Sleep(3000);
+            //
+            MyFuckingPoint.X = 745;
+            MyFuckingPoint.Y = 214;
+            label1.Location = MyFuckingPoint;
+            
+            Thread.Sleep(3000);
+            //
+            MyFuckingPoint.X = 112;
+            MyFuckingPoint.Y = 223;
+            label3.Location = MyFuckingPoint;
+            Thread.Sleep(3000);
+            //
+            MyFuckingPoint.X = 785;
+            MyFuckingPoint.Y = 158;
+            label2.Location = MyFuckingPoint;
+            Thread.Sleep(3000);
+            //
+            MyFuckingPoint.X = 795;
+            MyFuckingPoint.Y = 111;
+            label3.Location = MyFuckingPoint;
+            button1.Enabled = true;
+            button2.Enabled = true;
+
+        }
+        private void reiniciar()
+        {
+            passos = 0;
+            MyFuckingPoint.X = 112;
+            MyFuckingPoint.Y = 102;
+            label3.Location = MyFuckingPoint;
+
+
+            //levar  meio médio
+            MyFuckingPoint.X = 102;
+            MyFuckingPoint.Y = 149;
+            label2.Location = MyFuckingPoint;
+
+            //
+            MyFuckingPoint.X = 62;
+            MyFuckingPoint.Y = 206;
+            label1.Location = MyFuckingPoint;
+            label4.Text = passos.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reiniciar();
+        }
+
+
+        
+
+        private void label3_MouseUp(object sender, MouseEventArgs e)
+        {
+            MyFuckingPoint = FirstPoint3;
+            if (label3.Location.X <= 260)
+            {
+                if (label2.Location.X <= 260)
+                {
+                    if (label1.Location.X <= 260)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint.X = 112;
+                        MyFuckingPoint.Y = 102;
+                        label3.Location = MyFuckingPoint;
+                        BackupPoint[2] = MyFuckingPoint;
+                        passos++;
+                        label4.Text = passos.ToString();
+                        return;
+                    }
+                    MyFuckingPoint.X = 112;
+                    MyFuckingPoint.Y = 149;
+                    BackupPoint[2] = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                    label3.Location = MyFuckingPoint;
+                }
+                
+                else
+                {
+                    MyFuckingPoint.X = 112;
+                    if (label1.Location.X <= 260)
+                        MyFuckingPoint.Y = 149;
+                    else
+                        MyFuckingPoint.Y = 206;
+                    BackupPoint[2] = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                    label3.Location = MyFuckingPoint;
+                }
+
+            }
+            else if (label3.Location.X > 260 && label3.Location.X < 590)
+            {
+                if (label2.Location.X > 260 && label2.Location.X < 590)
+                {
+                    if (label1.Location.X > 260 && label1.Location.X < 590)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint.X = 431;
+                        MyFuckingPoint.Y = 102;
+                        label3.Location = MyFuckingPoint;
+                        BackupPoint[2] = MyFuckingPoint;
+                        passos++;
+                        label4.Text = passos.ToString();
+                        return;
+                    }
+                    MyFuckingPoint.X = 431;
+                    MyFuckingPoint.Y = 149;
+                    BackupPoint[2] = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                    label3.Location = MyFuckingPoint;
+                }
+
+                else
+                {
+                    MyFuckingPoint.X = 431;
+                    if (label1.Location.X > 260 && label1.Location.X < 590)
+                        MyFuckingPoint.Y = 149;
+                    else
+                        MyFuckingPoint.Y = 206;
+                    BackupPoint[2] = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                    this.label3.Location = MyFuckingPoint;
+                }
+
+            }
+            else
+            {
+                if (label2.Location.X >= 590)
+                {
+                    if (label1.Location.X >= 590)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint.X = 795;
+                        MyFuckingPoint.Y = 102;
+                        label3.Location = MyFuckingPoint;
+                        BackupPoint[2] = MyFuckingPoint;
+                        passos++;
+                        label4.Text = passos.ToString();
+                        if (passos == 7)
+                            MessageBox.Show("Parabéns você venceu com a soulução ótima!");
+                        else
+                            MessageBox.Show("Muito Bem você venceu!");
+                        return;
+                    }
+                    MyFuckingPoint.X = 795;
+                    MyFuckingPoint.Y = 149;
+                    BackupPoint[2] = MyFuckingPoint;
+                    label3.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                }
+
+                else
+                {
+                    MyFuckingPoint.X = 795;
+                    if (label1.Location.X >= 590)
+                        MyFuckingPoint.Y = 149;
+                    else
+                        MyFuckingPoint.Y = 206; 
+                    BackupPoint[2] = MyFuckingPoint;
+                    this.label3.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                }
+            }
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (label1.Location.X + 40 != label2.Location.X && label1.Location.X + 50 != label3.Location.X)
+            {
+                if (label1.Location.X <= 260)
+                {
+                    if (label2.Location.X <= 260 || label3.Location.X <= 260)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint = BackupPoint[0];
+                        label1.Location = MyFuckingPoint;
+                        return;
+
+                    }
+                    MyFuckingPoint.X = 62;
+                    MyFuckingPoint.Y = 206;
+                    BackupPoint[0] = MyFuckingPoint;
+                    label1.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                }
+                else if (label1.Location.X > 260 && label1.Location.X < 590)
+                {
+                    if (label2.Location.X > 260 && label2.Location.X < 590 || label3.Location.X > 260 && label3.Location.X < 590)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint = BackupPoint[0];
+                        label1.Location = MyFuckingPoint;
+                        return;
+
+                    }
+                    MyFuckingPoint.X = 381;
+                    MyFuckingPoint.Y = 206;
+                    BackupPoint[0] = MyFuckingPoint;
+                    label1.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+
+                }
+                else
+                {
+                    if (label2.Location.X >= 590 || label3.Location.X >= 590)
+                    {
+
+                        MyFuckingPoint = BackupPoint[0];
+                        label1.Location = MyFuckingPoint;
+                        return;
+
+                    }
+                    MyFuckingPoint.X = 745;
+                    MyFuckingPoint.Y = 206;
+                    BackupPoint[0] = MyFuckingPoint;
+                    label1.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                }
+            }
+        }
+        private void label2_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (label3.Location.X+10 != label2.Location.X)
+            {
+                MyFuckingPoint = FirstPoint2;
+                if (label2.Location.X <= 260)
+                {
+                    if (label3.Location.X <= 260)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint = BackupPoint[1];
+                        label2.Location = MyFuckingPoint;
+                        return;
+
+                    }
+                    MyFuckingPoint.X = 102;
+                    if (label1.Location.X <= 260)
+                        MyFuckingPoint.Y = 149;
+                    else
+                        MyFuckingPoint.Y = 206;
+                    BackupPoint[1] = MyFuckingPoint;
+                    label2.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                }
+                else if (label2.Location.X > 260 && label2.Location.X < 590)
+                {
+                    if (label3.Location.X > 260 && label3.Location.X < 590)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint = BackupPoint[1];
+                        label2.Location = MyFuckingPoint;
+                        return;
+
+                    }
+                    MyFuckingPoint.X = 421;
+                    if (label1.Location.X > 260 && label1.Location.X < 590)
+                        MyFuckingPoint.Y = 149;
+                    else
+                        MyFuckingPoint.Y = 206;
+                    BackupPoint[1] = MyFuckingPoint;
+                    label2.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+
+                }
+                else
+                {
+                    if (label3.Location.X >= 590)
+                    {
+
+                        //levar no alto
+                        MyFuckingPoint = BackupPoint[1];
+                        label2.Location = MyFuckingPoint;
+                        return;
+
+                    }
+                    MyFuckingPoint.X = 785;
+                    if (label1.Location.X >= 590)
+                        MyFuckingPoint.Y = 149;
+                    else
+                        MyFuckingPoint.Y = 206;
+                    BackupPoint[1] = MyFuckingPoint;
+                    label2.Location = MyFuckingPoint;
+                    passos++;
+                    label4.Text = passos.ToString();
+                }
+            }
+        }
+
+    }
+    
+}
